421fc66a8aab99f55c6fcbdaf6dd7b11c1251cc1
Bo Zhang <bo@gradle.com>
2019-05-18 20:56:19 +0800

    Fix failed tests

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,7 +1,7 @@
 package com.github.hcsp;
 
 public class Main {
-    private static String message = "Hello";
+    public static String message = "Hello";
 
     public static void main(String[] args) {
         System.out.println(message);
