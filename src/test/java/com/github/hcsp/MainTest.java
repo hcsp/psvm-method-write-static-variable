903271c009df3a6c715a31e04eb9d8ae8abc191c
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:06 +0800

    Update repo

--- src/test/java/com/github/hcsp/MainTest.java
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -1,14 +1,13 @@
 package com.github.hcsp;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
     @Test
     public void updateStaticVariableTest() {
-        assertEquals(Main.message.toLowerCase(), "hello");
+        Assertions.assertEquals(Main.message.toLowerCase(), "hello");
         Main.updateMessage();
-        assertEquals(Main.message.toLowerCase(), "hi");
+        Assertions.assertEquals(Main.message.toLowerCase(), "hi");
     }
 }
