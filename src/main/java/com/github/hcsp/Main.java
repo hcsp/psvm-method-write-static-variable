7fa63b18c7b3de8fffa050020557cb2b06ce9547
llaobin0207 <llaobin0207@163.com>
2019-11-29 09:30:41 +0800

    Java程序的基本结构练习：修改静态成员变量

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -12,5 +12,6 @@ public class Main {
     public static void updateMessage() {
         // Modify the static variable "message" to "Hi"
         // 将静态成员变量"message"的值改成"Hi"
+           message = "Hi";
     }
 }
