e853d94643eff1b5de142549723bbceccaee27ae
zhuxueqiao <mya_dong@126.com>
2020-02-15 00:03:55 +0800

    修改成员变量为Hi

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,7 +1,7 @@
 package com.github.hcsp;
 
 public class Main {
-    public static String message = "Hello";
+    public static String message = "hi";
 
     public static void main(String[] args) {
         System.out.println(message);
