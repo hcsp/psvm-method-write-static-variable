86752304992b7b4e237a4cdea7bb2249303d5c2c
chrisblack <zwhzhangweihan@gmail.com>
2020-05-25 18:18:23 -0400

    修改静态成员变量

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,7 +1,7 @@
 package com.github.hcsp;
 
 public class Main {
-    public static String message = "Hhh";
+    public static String message = "Hi";
 
     public static void main(String[] args) {
         System.out.println(message);
