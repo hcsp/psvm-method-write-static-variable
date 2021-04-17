a7f91eac643e18dff136ae8417c6a9bdfb6d89f5
sltap <wuzhenan00@gmail.com>
2021-04-17 11:33:44 +0800

    Hi

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,7 +1,7 @@
 package com.github.hcsp;
 
 public class Main {
-    public static String message = "Hi";
+    public static String message = "Hello";
 
     public static void main(String[] args) {
         System.out.println(message);
@@ -9,8 +9,9 @@ public class Main {
         System.out.println(message);
     }
 
-    public static void updateMessage() {
+    static void updateMessage() {
         // Modify the static variable "message" to "Hi"
         // 将静态成员变量"message"的值改成"Hi"
+        message="Hi";
     }
 }
