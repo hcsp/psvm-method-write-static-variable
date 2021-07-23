a8a1778a450522de762d6b0ee3784c38c268597c
ljx <1249192500@qq.com>
2021-07-23 16:42:15 +0800

    修改静态成员变量

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,7 +3,6 @@ package com.github.hcsp;
 public class Main {
     public static String message = "Hello";
 
-
     public static void main(String[] args) {
         System.out.println(message);
         updateMessage();
