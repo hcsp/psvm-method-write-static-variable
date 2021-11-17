ebd087e5c4b3b9f08c16079c8699daf26f2aa38a
Hao Jing <92510575+hitjh@users.noreply.github.com>
2021-11-17 19:35:09 +0800

    修改成员变量 (#289)
    修改成员变量 (#289)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,7 +2,6 @@ package com.github.hcsp;
 
 public class Main {
     public static String message = "Hello";
-
     public static void main(String[] args) {
         System.out.println(message);
         updateMessage();
@@ -12,5 +11,6 @@ public class Main {
     public static void updateMessage() {
         // Modify the static variable "message" to "Hi"
         // 将静态成员变量"message"的值改成"Hi"
+        message = "Hi";
     }
 }
