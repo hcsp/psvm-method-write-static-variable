688712a4dba903c7b7150c9c951e300aafdc884b
yzh <yzhyzh789@gmail.com>
2022-02-04 18:42:01 +0800

    update

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -5,11 +5,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(message);
-        updateMessage();
+        updateMessage(message);
         System.out.println(message);
     }
 
-    public static void updateMessage() {
+    public static void updateMessage(String message) {
+        Main.message = "hi";
         // Modify the static variable "message" to "Hi"
         // 将静态成员变量"message"的值改成"Hi"
     }
