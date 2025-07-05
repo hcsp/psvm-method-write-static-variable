b5632f94a94bd1e01aedbb72d804e1dcc37195c0
lyra <lyraaaaaaaaaaa@gmail.com>
2025-07-05 21:20:19 +1000

    I've finished the updateMessage method.

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,16 +1,17 @@
 package com.github.hcsp;
 
-public class Main {
-    public static String message = "Hello";
+public class Main {           // Main类
+    public static String message = "Hello";      //Main类的成员变量 message
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {        //程序入口
         System.out.println(message);
-        updateMessage();
+        updateMessage();                 //函数调用
         System.out.println(message);
     }
 
-    public static void updateMessage() {
+    public static void updateMessage() {         //函数声明
         // Modify the static variable "message" to "Hi"
         // 将静态成员变量"message"的值改成"Hi"
+        message = "Hi";
     }
 }
